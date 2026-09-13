b67972a57af9d965102d45f68bbbfaf5020d76da
Yang Yang <yy5478@hotmail.com>
2026-09-14 05:00:21 +0800

    fix(doctor): report current schema version

--- cmd/chora/workbench_doctor.go
+++ cmd/chora/workbench_doctor.go
@@ -26,7 +26,7 @@ import (
 	_ "modernc.org/sqlite"
 )
 
-const workbenchSchemaVersion = 42
+const workbenchSchemaVersion = 43
 
 var diagnosticVersionPattern = regexp.MustCompile(`(?:^|[^0-9A-Za-z_])(?:go|v)?(\d{1,4})\.(\d{1,4})(?:\.(\d{1,4}))?\b`)
 
